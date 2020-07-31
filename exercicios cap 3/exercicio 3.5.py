92831bb9b46287b9c4bba8bf324fa79d13c6910f
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-07-31 15:48:14 -0300

    Exercicio feito e comentado

---
+++ exercicios cap 3/exercicio 3.5.py
@@ -0,0 +1,15 @@
+# Calcule o resultado da expressão A > B and C or D, utilizando os valores da tablea a seguir
+
+#   A       B       C       D
+#   1       2       True    False
+#  10       3       False    False
+#   5       1       True    True
+
+
+A = 5
+B = 1
+C = True
+D = True
+
+
+print(A > B and C or D)
