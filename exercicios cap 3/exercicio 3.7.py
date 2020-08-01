1924fa0cceb40f4f6267acca6b316181b568a95d
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-01 19:10:26 -0300

    Exercicio feito e comentado

---
+++ exercicios cap 3/exercicio 3.7.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+# Input dos dois números
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Mostrar na tela o primeiro e segundo número - "\n" para pular linha
+print("O primeiro número é: %d \n"
+      "E o segundo número é: %d" % (num1, num2))
