36e66ebe7eb3f1e1864b612f67beaa0c5404e168
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-01 21:59:26 -0300

    Exercicio feito e comentado

---
+++ exercicios cap 3/exercicio 3.8.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+
+# 1 metro = 1000 milimetros
+
+comprimento = int(input("Digite o valor do comprimento: "))
+
+milimetros = comprimento * 1000
+
+print("Foi digitado %d metros. Que em milimetros são: %d" % (comprimento, milimetros))
