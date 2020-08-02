19432021c3098d6a72155fa9bde259d523d71b55
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-02 20:18:16 -0300

    Update exercicio 3.12.py

---
+++ exercicios cap 3/exercicio 3.12.py
@@ -0,0 +1,7 @@
+# Escreva um programa que calcule o tempo de uma viagem de carro.
+# Pergunte a distancia a percorrer e a velocidade média esperada para viagem.
+
+distancia = int(input("Digite a distancia a percorrer: (em km)"))
+velocidade_media = int(input("Quantos km/h de média? "))
+
+tempo = 
