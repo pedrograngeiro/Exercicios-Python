5e10e77611ade2df96a69b1d022b6e76832b5466
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-02 19:43:58 -0300

    Exercicio feito e comentado

--- exercicios cap 3/exercicio 3.12.py
+++
@@ -1,8 +0,0 @@
-# Escreva um programa que converta uma temperatura digitada em ºC em ºF. A fórmula para essa conversão é:
-# F = (9 x C / 5) + 32 # Formula para converter graus para fahrenheit
-
-C = int(input("Digite a temperatura em graus: "))
-
-F = (9 * C / 5) + 32
-
-print("A temperatura em Graus é igual a: %dºC. E em fahrenheit %dºF." % (C, F))
--- exercicios cap 3/exercicio 3.13.py
+++ exercicios cap 3/exercicio 3.13.py
@@ -1,11 +1,8 @@
-# Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo
-# usuário, assim como a quantidade de dias pelos quais o carro foi alugado.
-# Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por km.
+# Escreva um programa que converta uma temperatura digitada em ºC em ºF. A fórmula para essa conversão é:
+# F = (9 x C / 5) + 32 # Formula para converter graus para fahrenheit
 
-quilometros = float(input("Digite a quantidade de km percorridos."))
-dias = int(input("Por quantos dias o carro foi alugado: "))
-# deixei a variável preco como float, para declarar números com casas decimais.
-preco = float((0.15 * quilometros) + (60 * dias))
+C = int(input("Digite a temperatura em graus: "))
 
-print("Foi percorrido %d Km em %d dias." % (quilometros, dias))
-print("O preço é: R$%5.2f" % (preco))
+F = (9 * C / 5) + 32
+
+print("A temperatura em Graus é igual a: %dºC. E em fahrenheit %dºF." % (C, F))
