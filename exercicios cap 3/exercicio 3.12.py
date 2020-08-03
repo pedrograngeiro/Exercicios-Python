3eb0d6142ade34be6f61f3fc206182d7b249a58a
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-02 23:09:42 -0300

    Exercicio feito e comentado

--- exercicios cap 3/exercicio 3.12.py
+++ exercicios cap 3/exercicio 3.12.py
@@ -1,7 +1,12 @@
 # Escreva um programa que calcule o tempo de uma viagem de carro.
 # Pergunte a distancia a percorrer e a velocidade média esperada para viagem.
+# tempo = distancia / velocidade media
 
 distancia = int(input("Digite a distancia a percorrer: (em km)"))
 velocidade_media = int(input("Quantos km/h de média? "))
 
-tempo = 
+tempo = distancia / velocidade_media
+
+#Variavel tempo sendo convertida para minutos
+tempo = tempo * 60
+print("O tempo de vigem é de %d minutos." % (tempo))
