d98a3259788d4a93482ac8117d54fb16ad11e8bc
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-04 19:53:27 -0300

    Exercicio feito

---
+++ exercicios cap 4/exercicio 4.1.py
@@ -0,0 +1,8 @@
+# Analise o programa da listagem 4.2. Responda o que acontece se o primeiro e o segundo valores forem iguais?
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+
+if a > b:
+    print("O primeiro número é o maior!")
+if b > a:
+    print("O segundo número é o maior!")
