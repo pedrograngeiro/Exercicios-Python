341fdc9882e66b1dd8c1502115da9fcaea6038cb
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-08 03:50:02 -0300

    Create exercicio 4.5.py

---
+++ exercicios cap 4/exercicio 4.5.py
@@ -0,0 +1,4 @@
+# Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. Calcule o preço
+# da passagem, cobrando R$ 0,50 por km para viagens de até de 200 km, e R$ 0,45 para viagens mais longas.
+
+distancia = int(input"Digite a distancia em km a percorrer: ")
