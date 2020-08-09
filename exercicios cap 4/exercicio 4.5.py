e1987d63449a82b77098b5b2ca28b5b0e98be0fe
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-08 22:07:42 -0300

    Exercicio completo

--- exercicios cap 4/exercicio 4.5.py
+++ exercicios cap 4/exercicio 4.5.py
@@ -1,12 +1,8 @@
-# Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. Calcule o preço
-# da passagem, cobrando R$ 0,50 por km para viagens de até de 200 km, e R$ 0,45 para viagens mais longas.
+# Execute o programa (Listagem 4.5) e experimente alguns valores.
+# Verifique se os resultados foram os mesmos do programa anterior(Listagem 4.3)
 
-distancia = float(input("Digite a distancia em km a percorrer: "))
-preco = 0
-
-if distancia < 200:
-    preco = distancia * 0.50
-    print("A distancia foi: %d e o valor a ser pago é: R$ %5.2f" % (distancia, preco))
+idade = int(input("Digite a idade do seu carro: "))
+if idade <=3:
+    print("Seu carro é novo")
 else:
-    preco = distancia * 0.45
-    print("A distancia foi: %d e o valor a ser pago é: R$ %5.2f" % (distancia, preco))
+    print("Seu carro é velho")
