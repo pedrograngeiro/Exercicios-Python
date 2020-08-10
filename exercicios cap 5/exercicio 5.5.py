9e60eff5b654a9cb0b24b1fe091860c0bf077f20
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-10 01:42:08 -0300

    Exercicio completo

--- exercicios cap 5/exercicio 5.5.py
+++ exercicios cap 5/exercicio 5.5.py
@@ -3,7 +3,9 @@
 count = 1
 x = 1
 
+# count até 10 para mostrar os multiplos de 3
 while count < 10:
+    # se o modulo por 3 for igual a zero ele é multiplo
     if x % 3 == 0:
         print(x)
         count = count + 1
