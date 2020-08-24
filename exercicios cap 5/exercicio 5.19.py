8da06f6fa2f0234fbf81a891ceca7efc5535e920
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-08-24 20:13:18 -0300

    Exercicio parcial

--- exercicios cap 5/exercicio 5.19.py
+++ exercicios cap 5/exercicio 5.19.py
@@ -1,41 +1,56 @@
-# Modifique o programa para aceitar valores decimais, ou seja, também contar moedas de 0,01 0,02 0,05 0,10 e 0,50.
 
-valor = float(input("Digite o valor a pagar: "))
-cedulas = 0
-atual = 100.00
-apagar = valor
+print("=" * 30)
+print("{:^30}" . format("BANCO CEV"))
+print("=" * 30)
+
+# Parte cédula
+valor_dinheiro = int(input("Que valor você quer sacar? R$"))
+total = valor_dinheiro
+céd = 100
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f"Total de {totcéd} cédulas de R${céd}")
+        if céd == 100:
+            céd = 50
+        elif céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 5
+        elif céd == 5:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+
+# Parte moeda
+valor_moeda = int(input("Digite a parte da moeda: "))
+total = valor_moeda
+moeda = 50
+totmoeda = 0
 
 while True:
-    if atual <= apagar:
-        apagar = apagar - atual
-        cedulas = cedulas + 1
+    if total >= moeda:
+        total -= moeda
+        totmoeda += 1
     else:
-        if cedulas != 0:
-            print("%d cédulas(s) de R$%.2f" % (cedulas,atual))
-        if apagar == 0:
-            break
-        if atual == 100:
-            atual = 50
-        elif atual == 50:
-            atual = 20
-        elif atual == 20:
-            atual = 10
-        elif atual == 10:
-            atual = 5
-        elif atual == 5:
-            atual = 1
-        elif atual == 1:
-            atual = 0.50
-        elif atual == 0.50:
-            atual = 0.10
-        elif atual == 0.10:
-            atual = 0.05
-        elif atual == 0.05:
-            atual = 0.02
-        elif atual == 0.02:
-            atual = 0.01
-        elif atual == 0.01 and cedulas == 0:
-            break
+        if totmoeda > 0:
+            print(f"Total de {totmoeda} moedas de R${moeda}")
 
-
-        cedulas = 0
+        if moeda == 50:
+            moeda = 20
+        elif moeda == 20:
+            moeda = 10
+        elif moeda == 10:
+            moeda = 5
+        elif moeda == 5:
+            moeda = 1
+        totmoeda = 0
+        if total == 0:
+            break
