7a477195b91ecdd95934492a3b355ebb02784c73
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-09-04 22:51:08 -0300

    vevn add to git ignore

--- exercicios cap 5/exercicio 5.19.py
+++ exercicios cap 5/exercicio 5.19.py
@@ -1,10 +1,13 @@
-
 print("=" * 30)
 print("{:^30}" . format("BANCO CEV"))
 print("=" * 30)
+valor_float = float(input("Digite o valor que você quer sacar, se necessário digite quantos cents: "))
+valor_int = int(valor_float)
+print(valor_int)
+
 
 # Parte cédula
-valor_dinheiro = int(input("Que valor você quer sacar? R$"))
+valor_dinheiro = valor_int
 total = valor_dinheiro
 céd = 100
 totcéd = 0
@@ -24,33 +27,35 @@ while True:
         elif céd == 10:
             céd = 5
         elif céd == 5:
+            céd = 2
+        elif céd == 2:
             céd = 1
         totcéd = 0
         if total == 0:
             break
 
-# Parte moeda
-valor_moeda = int(input("Digite a parte da moeda: "))
-total = valor_moeda
-moeda = 50
-totmoeda = 0
-
+valor_float = valor_float - valor_int
+valor_float = valor_float * 100
+total = round(valor_float,2)
+céd = 50
+totcéd = 0
 while True:
-    if total >= moeda:
-        total -= moeda
-        totmoeda += 1
+    if total >= céd:
+        total = total - céd
+        totcéd = totcéd + 1
     else:
-        if totmoeda > 0:
-            print(f"Total de {totmoeda} moedas de R${moeda}")
-
-        if moeda == 50:
-            moeda = 20
-        elif moeda == 20:
-            moeda = 10
-        elif moeda == 10:
-            moeda = 5
-        elif moeda == 5:
-            moeda = 1
-        totmoeda = 0
+        if totcéd > 0:
+            print("Total de %d moedas de %d" % (totcéd, céd))
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 5
+        elif céd == 5:
+            céd = 2
+        elif céd == 2:
+            céd = 1
+        totcéd = 0
         if total == 0:
             break
