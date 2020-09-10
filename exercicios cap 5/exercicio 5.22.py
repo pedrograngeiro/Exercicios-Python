62ae808a307188f7148b0005af61d80f1f88dbf2
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-09-09 23:09:38 -0300

    menu funcional

--- exercicios cap 5/exercicio 5.22.py
+++ exercicios cap 5/exercicio 5.22.py
@@ -10,22 +10,34 @@ print("Digite 0 para sair.")
 
 while True:
     operacao = input("Digite a operação desejada: ")
-    n1 = float(input("Digite o primeiro número: "))
-    n2 = float(input("Digite o segundo número: "))
+
     resultado = 0
 
+    print(operacao)
+
     if operacao == "+":
+        n1 = float(input("Digite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
         resultado = n1 + n2
         print("A soma entre %5.2f e %5.2f é: %5.2f " % (n1, n2, resultado))
 
     if operacao == "-":
+        n1 = float(input("Digite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
         resultado = n1 - n2
         print("A subtração entre %5.2f e %5.2f é: %5.2f " % (n1, n2, resultado))
 
     if operacao == "*":
+        n1 = float(input("Digite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
         resulado = n1 * n2
         print("A multiplicação entre %5.2f e %5.2f é: %5.2f " % (n1, n2, resultado))
 
     if operacao == "/":
+        n1 = float(input("Digite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
         resulado = n1 / n2
         print("A divisão entre %5.2f e %5.2f é: %5.2f " % (n1, n2, resultado))
+
+    if operacao == "0":
+        break
