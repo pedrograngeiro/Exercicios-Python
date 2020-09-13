1152e736d72827d05f88b2765060737589a88abd
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-09-13 20:09:42 -0300

    Finalizado e comentado

---
+++ exercicios cap 5/exercicio 5.24.py
@@ -0,0 +1,18 @@
+# Modifique o programa anterior de forma a ler um número n.
+# Imprima os n primeiros números primos.
+
+núm = int(input("Digite um número: "))
+while True:
+    tot = 0
+    cont = 1
+    while cont <= núm: # cont vai percorrer todos os números abaixo de núm
+        if núm % cont == 0:
+            tot = tot + 1 # se tot igual a 2 ele é primo
+
+        cont+=1
+    if tot ==2: # mostra o número primo em tela
+        print(núm)
+
+    núm -=1 # variavel que faz percorrer todos os números abaixo de núm
+    if núm == 0: # encerra a variavel
+        break
