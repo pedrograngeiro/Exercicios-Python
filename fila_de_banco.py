c14d0b3081cf3c654cf32f9fcbb1e97528782659
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-11-23 16:38:40 -0300

    Add files via upload

---
+++ fila_de_banco.py
@@ -0,0 +1,23 @@
+ultimo = 10
+fila = list(range(1,ultimo+1))
+count = int(0)
+operacao = raw_input("Digite o comando: ")
+while True:
+    print("\n Existem % clientes na fila" % len(fila))
+    print("Fila atual: " , fila)
+    print("Digite F para adicionar um cliente ao fim da lista,")
+    print("Ou A para realizar o atendimento um cliente (S para sair)")
+    if operacao [count: count+1] == "A":
+        if(len(fila))>0:
+            atendido = fila.pop(0)
+            print("Cliente % d atendido" % atendido)
+        else:
+            print("Fila vazia! Ninguem para atender")
+    elif operacao [count: count+1] == "F":
+        ultimo = ultimo + 1 #incrementa o ticket novo
+        fila.append(ultimo)
+    elif operacao [count: count+1] == "S":
+        break
+    else:
+        print("Operacao invalida! Digite apenas F, A e S!")
+    count = count + 1
